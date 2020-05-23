49cf21fadd324dc75d5e0127677fa87a92404b99
Harmeet Kaur <33204062+Harmeet-Walia@users.noreply.github.com>
2020-05-22 22:29:08 -0700

    Ready for CI

--- src/main/java/com/PracticeProject/Pages/ResultPage.java
+++ src/main/java/com/PracticeProject/Pages/ResultPage.java
@@ -1,38 +1,42 @@
 package com.PracticeProject.Pages;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ResultPage {
 	WebDriver driver;
-	
-	
-//	@FindBy(xpath = "//div[contains(text(),'Important messages:')]")
-//	WebElement closeDialogBox;
-	
-	@FindBy(xpath = "//div[contains(@aria-label,' number 1:')]")
-	//@FindBy(xpath="div[contains(@id, '-feature-score')]")
-	WebElement closeDialogBox;
-	
-	
-	public ResultPage(WebDriver driver) {
-		this.driver=driver;
-		PageFactory.initElements(driver, this);
-		
-	}
-	
-	
 
-	public void closeAlert() {
+//	@FindBy(xpath = "//div[contains(@aria-label,' number 1:')]")
+//	@FindBy(xpath = "//button[contains(@id,'-dialog-close')]")
+	// @FindBy(xpath="div[contains(@id, '-feature-score')]")
+	// @FindBy(xpath = "//div[contains(text(),'Important messages:')]")
+//	WebElement closeDialogBox;
+
+	public ResultPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void closeAlert() throws InterruptedException {
+		WebElement element = driver.findElement(By.xpath("//div[contains(@aria-label,' number 1:')]"));
+//		WebDriverWait wait=new WebDriverWait(driver, 10);
+//		wait.until(ExpectedConditions.elementToBeClickable(element));
+		Thread.sleep(2000);
+
 		Actions actions = new Actions(driver);
-		actions.pause(2000)
-				.click(closeDialogBox)
-				.build()
-				.perform();
+		actions.pause(1000).click(element).build().perform();
 
 	}
 
--- src/test/java/com/newProject/PracticeProject/TestSearchFlight.java
+++ src/test/java/com/newProject/PracticeProject/TestSearchFlight.java
@@ -1,6 +1,8 @@
 package com.newProject.PracticeProject;
 
 
+import java.awt.AWTException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
@@ -14,7 +16,7 @@ public class TestSearchFlight  extends BaseTest {
 	
 	
 	@Test(dataProvider="getCellData")
-	public void searchAFlight(String Origin, String Destination, String departureDate, String returndate)
+	public void searchAFlight(String Origin, String Destination, String departureDate, String returndate) throws AWTException, InterruptedException
  {
 		searchPage search=new searchPage(driver);
 		ResultPage result=new ResultPage(driver);
@@ -24,11 +26,6 @@ public class TestSearchFlight  extends BaseTest {
 		search.enterReturnDate(returndate);
 		search.submit();
 		result.closeAlert();
-		
-		
-		
-		
-		
 	}
 	
 	@DataProvider
