5bc39c06dbaecdd5ef53798a19731ae90618fb0d
Harmeet Kaur <33204062+Harmeet-Walia@users.noreply.github.com>
2020-05-29 22:13:29 -0700

    Added Selenium Driver folder in resources

--- src/test/java/com/newProject/PracticeProject/BaseTest.java
+++ src/test/java/com/newProject/PracticeProject/BaseTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -20,19 +21,22 @@ public class BaseTest {
 	@BeforeMethod
 	@Parameters("browser")
 	
-	public void initiateDriver(@Optional("chrome") String browser) throws Exception{
+	public void initiateDriver(@Optional("FireFox")  String browser) throws Exception{
 		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\walia\\SeleniumDrivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\SeleniumDrivers\\chromedriver.exe");
+			
 			driver=new ChromeDriver();
 			
 			driver.get("http://www.kayak.com");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
-//		} else if(browser.equals("FireFox")) {
-//			System.setProperty("webdriver.gecko.driver", "C:\\Users\\walia\\SeleniumDrivers\\chromedriver.exe");
-//			driver.get("http://www.kayak.com");
-//			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//			driver.manage().window().maximize();
+			
+		} else if (browser.equalsIgnoreCase("FireFox")) {
+			System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\SeleniumDrivers\\geckodriver.exe");
+			driver=new FirefoxDriver();
+			driver.get("http://www.kayak.com");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
 			
 			
 	}
